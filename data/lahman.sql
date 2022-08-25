662deb94eb276990753fb477b07d7cd6f3be4411
bryanacolson <bryanacolson@gmail.com>
2022-08-25 04:54:32 -0500

    3

--- data/lahman.sql
+++ data/lahman.sql
@@ -4,5 +4,17 @@ FROM teams;
 --2
 SELECT namegiven, height
 FROM people
-order by height ASC;
---3l
+order by height ASC
+LIMIT 1;
+--3
+SELECT DISTINCT(p.namegiven), s.schoolid, p.playerid, s.schoolname
+FROM collegeplaying as c
+INNER JOIN schools s
+on s.schoolid = c.schoolid
+INNER JOIN people as  p
+on s.schoolid = c.schoolid
+WHERE s.schoolname = 'Vanderbilt University';
+--4 
+
+
+
