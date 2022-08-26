c127292962f8277a571f064a314b1e071ecc77c2
bryanacolson <bryanacolson@gmail.com>
2022-08-25 20:59:41 -0500

    6

--- data/lahman.sql
+++ data/lahman.sql
@@ -15,6 +15,21 @@ INNER JOIN people as  p
 on s.schoolid = c.schoolid
 WHERE s.schoolname = 'Vanderbilt University';
 --4 
+SELECT pos
+from fielding;
+
+--5
+SELECT AVG(so)
+FROM batting;
+
+--6
+SELECT (AVG(f.sb)/AVG(f.cs))
+FROM fielding as f
+INNER JOIN people as p 
+ON f.playerid = p.playerid;
+
+--7
+SELECT 
 
 
 
