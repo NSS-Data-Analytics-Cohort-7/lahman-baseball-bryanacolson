854731e6b606651ddbb85582f998f7459246da09
bryanacolson <bryanacolson@gmail.com>
2022-08-27 13:18:11 -0500

    9

--- data/lahman.sql
+++ data/lahman.sql
@@ -60,22 +60,43 @@ ORDER BY (AVG(attendance/games))
 LIMIT 5;
 
 --9
-SELECT aw.playerid, aw.awardid
+SELECT aw.playerid, COUNT(aw.awardid)
 FROM awardsmanagers as aw
 INNER JOIN people as p
 ON p.playerid = aw.playerid
 WHERE awardid = 'TSN Manager of the Year'
 GROUP BY aw.playerid, aw.awardid
-ORDER BY COUNT(aw.playerid);
+ORDER BY COUNT(aw.playerid) DESC;
 --9bish
-SELECT aw.playerid, aw.awardid
+
+SELECT aw.playerid, COUNT(aw.awardid), t.lgid
 FROM awardsmanagers as aw
 INNER JOIN people as p
 ON p.playerid = aw.playerid
+INNER JOIN managershalf as mh
+ON p.playerid = mh.playerid
+INNER JOIN teams as t
+ON mh.yearid = t.yearid
 WHERE awardid = 'TSN Manager of the Year'
-GROUP BY aw.playerid, aw.awardid
-ORDER BY COUNT(aw.playerid);
+GROUP BY aw.playerid, aw.awardid, t.lgid
+ORDER BY COUNT(aw.playerid) DESC;
 
+--10
+select playerid, yearid, h
+FROM batting
+ORDER by h DESC;
 
-
+--11
+SELECT t.w, s.salary
+FROM salaries as s
+INNER JOIN managers as m
+on s.yearid = m.yearid
+INNER JOIN teams as t
+ON m.yearid=t.yearid
+INNER JOIN managershalf as mh
+ON  mh.yearid = t.yearid
+INNER JOIN people as p
+on p.playerid= mh.playerid
+GROUP by s.salary, t.w
+ORDER by s.salary;
 
